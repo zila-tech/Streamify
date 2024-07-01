5fa7c88bfea849cefaf64f5343d90b82407acaf8
zila-tech <info.zilatech@gmail.com>
2024-07-01 20:33:24 +0000

    Install accounts app in settings.py

--- PaulVideoPlatform/settings.py
+++ PaulVideoPlatform/settings.py
@@ -14,15 +14,16 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DEFAULT_APPS=[
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+    'django.contrib.staticfiles',]
+
+LOCAL_APPS=['accounts']
+INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
