632a9f13b319a59f790e913af095edd3edb60570
zila-tech <info.zilatech@gmail.com>
2024-07-01 16:29:25 +0000

    Configure static and media file settings in settings.py

--- PaulVideoPlatform/settings.py
+++ PaulVideoPlatform/settings.py
@@ -1,9 +1,7 @@
-
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -100,9 +98,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_URL = 'static/'
+# Media files (Uploaded files)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Optionally, if using Whitenoise for serving static files in production:
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ static/js/pages/toastr.js
@@ -0,0 +1,14 @@
+function showToast(heading, text, icon) {
+  console.log('Hi dey');
+  $.toast({
+    heading: heading,
+    text: text,
+    position: 'top-right',
+    loaderBg: '#ff6849',
+    icon: icon,
+    hideAfter: 3500,
+    stack: 6,
+  });
+}
+
+console.log('Hi welcome');
