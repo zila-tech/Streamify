b408fa2c143ff56b6441ae567db24e13f8d98694
zila-tech <info.zilatech@gmail.com>
2024-07-01 23:18:09 +0000

    Set up URLs for login,activate account and logout of accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import userregistrationview
+from .views import userregistrationview, activateaccountview, loginview, logoutview
 
 app_name = "auth"
 urlpatterns = [
     path("signup/", userregistrationview, name="signup"),
+    path("activate/<uidb64>/<token>/", activateaccountview, name="activate"),
+    path("login/", loginview, name="login"),
+    path("logout/", logoutview, name="logout"),
 ]
+
