1ef5ca57ed0c5f712a634bb836640dce8f6c35db
zila-tech <info.zilatech@gmail.com>
2024-07-02 02:05:09 +0000

    Set urls for forgot pwd, pwd reset complete, pwd confirmation in accounts app views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import userregistrationview, activateaccountview, loginview, logoutview
+from .views import (
+    userregistrationview,
+    activateaccountview,
+    loginview,
+    logoutview,
+    forgotpasswordview,
+    passwordresetcompleteview,
+    passwordresetconfirmview,
+)
+
 
 app_name = "auth"
 urlpatterns = [
@@ -7,5 +16,15 @@ urlpatterns = [
     path("activate/<uidb64>/<token>/", activateaccountview, name="activate"),
     path("login/", loginview, name="login"),
     path("logout/", logoutview, name="logout"),
+    path("forgot-password/", forgotpasswordview, name="forgotPassword"),
+    path(
+        "reset-password/<uidb64>/<token>/",
+        passwordresetconfirmview,
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset-password-complete/",
+        passwordresetcompleteview,
+        name="password_reset_complete",
+    ),
 ]
-
