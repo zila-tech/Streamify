94d050ea234d5f1f3c630bf6e8d4fbf4b5ac1f28
zila-tech <info.zilatech@gmail.com>
2024-07-02 03:08:41 +0000

    Add Video  model for video storing and navigation

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Account
+
+
+class Video(models.Model):
+    """
+    Model representing a video.
+
+    Attributes:
+        title (str): Title of the video.
+        description (str): Description of the video.
+        video_file (FileField): The video file.
+        created_by (ForeignKey): The user who upload the video
+        uploaded_at (datetime): The date and time when the video was uploaded.
+    """
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    video_file = models.FileField(upload_to="videos/")
+    created_by  = models.ForeignKey(
+        Account,
+        related_name="owner",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
