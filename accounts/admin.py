8fa2f98ca8c24176d18aae2ec9c5cc8440b5aff7
zila-tech <info.zilatech@gmail.com>
2024-07-02 00:12:07 +0000

    Register accounts model in admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
+from accounts.models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Account
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "username",
+        "gender",
+        "last_login",
+        "date_joined",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "gender",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "gender",
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_admin",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "username",
+                    "gender",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_admin",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("email",)
