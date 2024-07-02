809e903f03ae548fca0a8d07e2ef0d81465d8e3b
zila-tech <info.zilatech@gmail.com>
2024-07-02 03:12:19 +0000

    Implement video functionality in default admin site

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_by", "uploaded_at")
+    search_fields = ("title", "description", "created_by__email")
+    list_filter = ("uploaded_at", "created_by")
+    readonly_fields = ("uploaded_at",)
+
+    fieldsets = (
+        (None, {"fields": ("title", "description", "video_file")}),
+        (
+            "Advanced options",
+            {
+                "classes": ("collapse",),
+                "fields": ("created_by", "uploaded_at"),
+            },
+        ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        obj.save()
+
+
+admin.site.register(Video, VideoAdmin)
