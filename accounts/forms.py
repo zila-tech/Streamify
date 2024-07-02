30bfab73386fb4c6c1fd00958b0f4573791e82fa
zila-tech <info.zilatech@gmail.com>
2024-07-02 01:10:56 +0000

    Change password clearn

--- accounts/forms.py
+++ accounts/forms.py
@@ -66,7 +66,7 @@ class RegistrationForm(forms.ModelForm):
             attrs={
                 "placeholder": "Enter password",
                 "class": "form-control ps-15 bg-transparent",
-             }
+            }
         ),
         strip=False,
         help_text=password_validation.password_validators_help_text_html(),
@@ -118,7 +118,7 @@ class RegistrationForm(forms.ModelForm):
             raise ValidationError("This email is already registered.")
         return email
 
-    def clean_new_password2(self):
+    def clean_confirm_password(self):
         password = self.cleaned_data.get("password")
         confirm_password = self.cleaned_data.get("confirm_password")
         if password and confirm_password and password != confirm_password:
@@ -128,14 +128,6 @@ class RegistrationForm(forms.ModelForm):
             )
         password_validation.validate_password(confirm_password, self.user)
         return confirm_password
-
-    def clean(self):
-        cleaned_data = super(RegistrationForm, self).clean()
-        password = cleaned_data.get("password")
-        confirm_password = cleaned_data.get("confirm_password")
-
-        if password != confirm_password:
-            raise forms.ValidationError("Passwords do not match")
 
 
 class ForgotPasswordForm(forms.Form):
