bcd66516f88da77cc32a348c7729a40200a55068
zila-tech <info.zilatech@gmail.com>
2024-07-06 20:35:14 +0000

    feat: Update Video model and add default thumbnail functionality

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,7 @@
+import random
 from django.db import models
-
-from accounts.models import Account
-
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 class Video(models.Model):
     """
@@ -11,21 +11,33 @@ class Video(models.Model):
         title (str): Title of the video.
         description (str): Description of the video.
         video_file (FileField): The video file.
-        created_by (ForeignKey): The user who upload the video
+        created_by (ForeignKey): The user who uploaded the video.
         uploaded_at (datetime): The date and time when the video was uploaded.
     """
 
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    video_file = models.FileField(upload_to="videos/")
-    created_by  = models.ForeignKey(
-        Account,
+    title = models.CharField(_("Title"), max_length=255)
+    description = models.TextField(_("Description"))
+    video_file = models.FileField(_("Video File"), upload_to="videos/")
+    created_by = models.ForeignKey(
+        "Account",
         related_name="owner",
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
     )
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    thumbnail = models.ImageField(
+        _("Thumbnail"), upload_to="thumbnails/", null=True, blank=True
+    )
+    uploaded_at = models.DateTimeField(_("Uploaded At"), auto_now_add=True)
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            return (
+                settings.STATIC_URL
+                + f"images/default_thumbnails/default_thumbnail-{random.randint(1, 5)}.jpg"
+            )
 
     def __str__(self):
         return self.title
