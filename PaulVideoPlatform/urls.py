e103e649fe36d2ce9ce94b09c9cf9999cea9d472
zila-tech <info.zilatech@gmail.com>
2024-07-10 12:24:11 +0000

    Config static and media urls in main project

--- PaulVideoPlatform/urls.py
+++ PaulVideoPlatform/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("accounts.urls")),
+    path("", include("videos.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
