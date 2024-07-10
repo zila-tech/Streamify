d896cffa35312546a08e1fb1c40814b7db2e3a87
zila-tech <info.zilatech@gmail.com>
2024-07-10 12:40:42 +0000

    Set urls path for video list, single, next, previous views

--- videos/urls.py
+++ videos/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, videodetailview, getprevnextidsview
 
 urlpatterns = [
     path("", home, name="home"),
+    path("videos/<int:pk>/", videodetailview, name="video-detail"),
+    path(
+        "videos/get-prev-next-ids/<int:pk>/",
+        getprevnextidsview,
+        name="get-prev-next-ids",
+    ),
 ]
