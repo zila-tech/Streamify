5262b574b00b49285971584887b0a8b9283d74bb
zila-tech <info.zilatech@gmail.com>
2024-07-12 04:19:57 +0000

    Update custom template tag, include update_registration_session to clear session after alert for verification

--- videos/templatetags/custom_filters.py
+++ videos/templatetags/custom_filters.py
@@ -7,3 +7,13 @@ register = template.Library()
 def gender_to_avatar(gender):
     value = 13 if gender == "Male" else 16
     return value
+
+
+@register.simple_tag(takes_context=True)
+def update_registration_session(context):
+    request = context["request"]
+    request.session["registration_success"] = False
+    if "registration_success" in request.session:
+        success = request.session.pop("registration_success")
+        return success
+    return False
